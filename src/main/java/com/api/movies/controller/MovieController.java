f53db0a52d752d1592bba724e60fbb1585e871f5
elouardyabderrahim <elouardy.abderrahim07@gmail.com>
2024-02-04 15:52:37 +0100

    feat: movies endpoint ,

--- src/main/java/com/api/movies/controller/MovieController.java
+++ src/main/java/com/api/movies/controller/MovieController.java
@@ -1,19 +1,36 @@
 package com.api.movies.controller;
 
 
+import com.api.movies.entities.Movie;
+import com.api.movies.service.MovieService;
+import org.bson.types.ObjectId;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movies")
 public class MovieController {
 
-    @GetMapping
-    public ResponseEntity<String> allMovies() {
+    private final MovieService movieService;
 
-        return new ResponseEntity<String>("all movies", HttpStatus.OK);
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Movie>> getAllMovies() {
+
+        return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
+    }
+
+
+    @GetMapping("/{imdbId}")
+    public ResponseEntity<Optional<Movie>> getMovieByImdbId(@PathVariable String imdbId) {
+
+        return new ResponseEntity<Optional<Movie>>(movieService.MovieByImdbId(imdbId), HttpStatus.OK);
     }
 }
