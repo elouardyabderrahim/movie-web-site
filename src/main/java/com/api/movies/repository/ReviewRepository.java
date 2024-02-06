1e775f49db3a02c4fdf081a550daa54b7bbea357
elouardyabderrahim <elouardy.abderrahim07@gmail.com>
2024-02-06 19:56:30 +0100

    feat:create repository

--- src/main/java/com/api/movies/repository/ReviewRepository.java
+++ src/main/java/com/api/movies/repository/ReviewRepository.java
@@ -1,4 +1,12 @@
 package com.api.movies.repository;
 
-public class ReviewRepository {
+import com.api.movies.entities.Review;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+
 }
